dda1ab5808bd4d76fa8cd4aea25beb36993f319b
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-02-05 17:09:56 -0500

    Fetch Movie List from TMDB API

--- src/utlis/constant.js
+++ src/utlis/constant.js
@@ -3,3 +3,11 @@ export const LOGO_URL = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_p
 export const USER_PHOTO = "https://i.pinimg.com/564x/1b/a2/e6/1ba2e6d1d4874546c70c91f1024e17fb.jpg";
 
 export const BACKGROUND_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/7a8c0067-a424-4e04-85f8-9e25a49a86ed/web/CA-en-20250120-TRIFECTA-perspective_1ea750e2-25c0-4073-971e-5179e54d8b41_large.jpg"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmRlZDM5M2NiMzgzODkyNDhjN2RmMTEwY2I5YmI2ZSIsIm5iZiI6MTcxOTQyNTIzMS42NDYwMDAxLCJzdWIiOiI2NjdjNThjZjNkNzAyZTUyYzU1NTdkMGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ps3f4EPmAbCDOxqZfZtPT7rl88vQL4OwbgWfJuQ4vv4'
+    }
+};
