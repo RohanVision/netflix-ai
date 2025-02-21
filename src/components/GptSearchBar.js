b7f7f827c457a44bd029f0cef7f500418297515b
Rohan Amberkar <amberkar.rohan17@gmail.com>
2025-02-21 18:53:39 -0500

    Multi Language Feature added

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import lang from "../utlis/languageConstant"
 
 const GptSearchBar = () => {
+    const langKey = useSelector(store => store.config.lang)
     return (
         <div className='bg-black p-40'>
             <form className='flex justify-center'>
-                <input type="text" className='w-1/3 flex-4 flex-grow-0 border px-4 py-2 outline-none' placeholder='What would you like to watch today?' />
-                <button className='bg-red-500 rounded-sm text-white px-4 py-2 flex-2'>Search</button>
+                <input type="text" className='w-1/2 flex-4 flex-grow-0 border px-4 py-2 outline-none'
+                    placeholder={lang[langKey].gptPlaceholder} />
+                <button className='bg-red-500 w-1/6 rounded-sm text-white px-4 py-2 flex-2'>{lang[langKey].search}</button>
             </form>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ import { addUser, removeUser } from "../utlis/userSlice";
 import { LOGO_URL } from "../utlis/constant";
 import { toggleGptSearchView } from "../utlis/gptSlice";
 import { SUPOORTED_LANGUAGE } from "../utlis/constant";
+import { changeLanguage } from "../utlis/configSlice";
 
 const Header = ({ isSignIn, setIsSignIn }) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const user = useSelector(store => store.user)
+    const user = useSelector(store => store.user);
+    const showGptSearch = useSelector(store => store.gpt.showGptSearch)
     const handleSignOut = () => {
         signOut(auth).then(() => {
         }).catch((error) => {
@@ -48,6 +50,10 @@ const Header = ({ isSignIn, setIsSignIn }) => {
         dispatch(toggleGptSearchView());
     }
 
+    const handleLanguageChange = (e) => {
+        dispatch(changeLanguage(e.target.value))
+    }
+
     return (
         <div className='absolute w-full px-52 py-2 bg-gradient-to-b from-black z-10 text-white'>
             <div className='flex items-center justify-between'>
@@ -58,10 +64,10 @@ const Header = ({ isSignIn, setIsSignIn }) => {
                     {!user && <button onClick={handleClick}
                         className='text-white text-sm px-6 py-2 rounded-md bg-red-600 hover:bg-red-700 transition delay-150 duration-300 ease-in-out'>{isSignIn ? 'Sign Up' : 'Sign In'}</button>}
                     {user && <div className="flex gap-2">
-                        <select className="bg-red-500 text-black font-semibold rounded-md p-2 cursor-pointer outline-none">
-                            {SUPOORTED_LANGUAGE.map(lang => <option className="cursor-pointer" value="en">English</option>)}
-                        </select>
-                        <button onClick={handleGptSearchClick} className="bg-purple-500 rounded-md px-4 py-2 mx-4">AI Search</button>
+                        {showGptSearch && <select className="bg-red-500 text-black font-semibold rounded-md p-2 cursor-pointer outline-none" onChange={handleLanguageChange}>
+                            {SUPOORTED_LANGUAGE.map(lang => <option key={lang.identifier} className="cursor-pointer" value={lang.identifier}>{lang.name}</option>)}
+                        </select>}
+                        <button onClick={handleGptSearchClick} className="bg-purple-500 rounded-md px-4 py-2 mx-4">{showGptSearch ? "Home" : "AI Search"}</button>
                         <img className="w-9 h-9" src={user?.photoURL} alt="userIcon" />
                         <button onClick={handleSignOut}>Sign Out</button>
                     </div>}
